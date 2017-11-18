e8a85d284a05e7c6b9f338f64962ccc9d524acc2
Farhad2015 <mrforhad1@gmail.com>
2017-11-18 15:24:09 +0600

    Complete Dice Roll Project

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -23,7 +23,40 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let randomDiceIndexOne = Int(arc4random_uniform(6)) + 1
+        let randomDiceIndexTwo = Int(arc4random_uniform(6)) + 1
         
+        if randomDiceIndexOne == 1 {
+            diceImageViewOne.image = UIImage(named: "dice1")
+        } else if randomDiceIndexOne == 2 {
+            diceImageViewOne.image = UIImage(named: "dice2")
+        } else if randomDiceIndexOne == 3 {
+            diceImageViewOne.image = UIImage(named: "dice3")
+        } else if randomDiceIndexOne == 4 {
+            diceImageViewOne.image = UIImage(named: "dice4")
+        } else if randomDiceIndexOne == 5 {
+            diceImageViewOne.image = UIImage(named: "dice5")
+        } else if randomDiceIndexOne == 6 {
+            diceImageViewOne.image = UIImage(named: "dice6")
+        }
+        
+        
+        if randomDiceIndexTwo == 1 {
+            diceImageViewTwo.image = UIImage(named: "dice1")
+        } else if randomDiceIndexTwo == 2 {
+            diceImageViewTwo.image = UIImage(named: "dice2")
+        } else if randomDiceIndexTwo == 3 {
+            diceImageViewTwo.image = UIImage(named: "dice3")
+        } else if randomDiceIndexTwo == 4 {
+            diceImageViewTwo.image = UIImage(named: "dice4")
+        } else if randomDiceIndexTwo == 5 {
+            diceImageViewTwo.image = UIImage(named: "dice5")
+        } else if randomDiceIndexTwo == 6 {
+            diceImageViewTwo.image = UIImage(named: "dice6")
+        }
+        
+        print("The first random number is \(randomDiceIndexOne)")
+        print("The second random number is \(randomDiceIndexTwo)")
     }
     
 }
